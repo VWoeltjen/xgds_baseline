2b0010e824e883612700b36018b75ff3ffcb5cf5
Tamar Cohen <tamar.cohen@nasa.gov>
2018-09-14 15:32:59 -0700

    set up files for yamlModels.py file, cleaned up index and added blank tests.py file

--- apps/xgds_baseline_app/yamlModels.py
+++ apps/xgds_baseline_app/yamlModels.py
@@ -13,8 +13,8 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 #__END_LICENSE__
+
 from __future__ import unicode_literals
 
 from django.db import models
-from yamlModels import *
-
+import xgds_timeseries.models as xgds_timeseries
